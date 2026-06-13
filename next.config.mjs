fee4f967b470bb9312ce739012937fcc269aeb62
kingpowertr <ultimatefake31@gmail.com>
2026-06-13 06:47:51 +0300

    asdas

--- next.config.mjs
+++ next.config.mjs
@@ -12,20 +12,12 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBypass: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    config.module.rules.push({
-      test: /\.(md|node)$/,
-      use: 'ignore-loader',
-    });
-    return config;
+ webpack: (config) => {
+  config.module.rules.push({
+    test: /\.md$/,
+    type: 'asset/source',
+  });
+  return config;
   },
 };
 
